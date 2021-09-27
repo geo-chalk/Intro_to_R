b4e394ca85155abf4a262693ba625a6f057e441a
jojoshulk <jojoshulk@gmail.com>
2021-09-28 02:23:11 +0300

    Update 28/09

--- answers.r
+++ answers.r
@@ -1,36 +1,41 @@
 library(foreign)
+library(sjPlot)
+library(sjmisc)
+library(psych)
 list.files(".")
 
 # 1. Load the data from the SPSS file.
 df <- read.spss("./salary.sav", to.data.frame=TRUE)
-head(df)
+describe(df)
 summary(df)
 
 # 2. Run a frequency table for "gender". Report how many men are in the sample. 
 # Which type of graph would best display data for a variable like gender?
+frq(df$sex, title = "Gender", out = "v")
+sprintf("The total number of men is: %d", sum(df$sex == 'MALES'))
 gender_freq <- table(df$sex)
-max_y <- max(gender_freq)
-png("Plots/2_gender_freq.png")
+png("Plots/2_gender_barplot.png")
 barplot(gender_freq, main = "Gender Frequency",col = c(4,2), axes = TRUE, border = TRUE)
-dev.off()     
+dev.off()
 
 # 3. Run a frequency table for "jobcat". 
 # Report the most common job category and its percentage.
-jobcat_freq <- table(df$jobcat)
-cbind(Count = jobcat_freq, Perc = sprintf("%05.2f%%",round(prop.table(jobcat_freq)*100, 2)))
+frq(df$jobcat,title = "Job Category", out = "v")
+sprintf("The total number of men is: %d", sum(df$sex == 'MALES'))
 
 # 4. Make a pie chart for "jobcat" 
 png("Plots/rplot.png")
 pie(jobcat_freq, col = 2:8)
-legend(0.9, 1, names(jobcat_freq), cex = 0.7, fill = 2:8)
+legend(0.7, 1, names(jobcat_freq), cex = 0.7, fill = 2:8)
 dev.off()
 
 # 5. report the percentage of people who are either clerical workers or security officers
-cler_sec <- sum(df$jobcat == c('CLERICAL', 'SECURITY OFFICER'))/length(df$jobcat)
+cler_sec <- sum((df$jobcat == 'CLERICAL')|(df$jobcat == 'SECURITY OFFICER'))/length(df$jobcat)
 sprintf("The percetage of people who are wither clerical workers or security officers is: %05.2f%%", cler_sec*100)
 
 # 6. Run a frequency distribution for “salnow”
+frq(df$salnow, title="salary", out = "v", auto.grp = 10)
 x <- table(df$salnow)
-factorx <-factor(cut(x, breaks=nclass.Sturges(x), dig.lab=5))
-as.matrix(table(factorx))
 hist(x, breaks=nclass.Sturges(x))
+
+
